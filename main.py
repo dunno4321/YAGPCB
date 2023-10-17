fa3f383b0e7aa482d242e4759687834720536071
dunno4321 <arduinoman289@gmail.com>
2023-10-16 20:15:40 -0400

    updated a lot

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from twitchio.ext import commands
 from flask import Flask, request
 from threading import Thread
 import requests
+import logging
 import random
 import json
 import time
@@ -11,35 +12,31 @@ app = Flask(__name__)
 has_token = False
 token_time = datetime.now() - timedelta(days=3)
 token = ""
-
-try:
-    with open("config.json") as file:
-        config = json.load(file)
-except FileNotFoundError:
-    print("config.json not found, please run set_stuff.py with the command 'python set_stuff.py'")
-tmp = list(config.keys())
-tmp.sort()
-if tmp != ['channel', 'client_id', 'client_secret']:
-    with open("config.json", "w") as file:
-        json.dump({"client_id": "", "client_secret": "", "channel": ""}, file, indent=2)
-    raise Exception("Invalid config.json format! Resetting to default...")
-if config["channel"] == "" or config["client_id"] == "" or config["client_secret"] == "":
-    raise Exception("Default config detected, please run set_stuff.py with the command 'python set_stuff.py' to configure the bot")
+with open("log.log", "w") as file:
+    file.write("")
+logging.basicConfig(filename="log.log", level=logging.DEBUG)
+logging.info("Hello world!")
 
 
 def parse(url_):
-    url_ = url_[url_.rfind("/") + 2:]
-    params2 = url_.split("&")
-    params = {}
-    for item in params2:
-        tmp = item.split("=")
-        params[tmp[0]] = tmp[1]
-    return params
+    logging.debug("Parsing URL: " + url_)
+    try:
+        url_ = url_[url_.rfind("/") + 2:]
+        params2 = url_.split("&")
+        params = {}
+        for item in params2:
+            tmp = item.split("=")
+            params[tmp[0]] = tmp[1]
+        return params
+    except Exception as e:
+        logging.error("Error parsing URL: " + str(e))
+        return {}
 
 
 @app.route("/")
 def home():
     global token_time, has_token, token
+    logging.debug("Request on " + request.url)
     params = parse(request.url)
     if "code" in list(params.keys()):
         has_token = True
@@ -47,14 +44,19 @@ def home():
         token_time = datetime.now()
         with open("token.json", "w") as file:
             json.dump({"token": token}, file)
+        logging.info("Successfully got token!")
         return "Successfully authorized!"
     else:
         has_token = False
         token = f"error: {params['error']}: {params['error_description']}"
+        logging.error(f"Error getting token: {token}")
+        print("Error getting token:")
+        print(token)
         return token
 
 
 def add_quote(ctx: commands.Context):
+    logging.info(f"Adding quote for streamer {ctx.channel.name}")
     with open("quotes.json") as file:
         tmp = json.load(file)
         tmp = [item for item in tmp if item["streamer"] == ctx.channel.name]
@@ -80,55 +82,113 @@ def format_quote(quote):
 
 
 def random_quote(ctx: commands.Context):
+    logging.info(f"Getting random quote for streamer {ctx.channel.name}")
     streamer = ctx.channel.name
     with open("quotes.json") as file:
         tmp = json.load(file)
     tmp = [quote for quote in tmp if quote["streamer"] == streamer]
+    if len(tmp) == 0:
+        logging.warn("No quotes found!")
     return format_quote(random.choice(tmp)) if len(tmp) > 0 else "No quotes found for this streamer!"
 
 
-def get_access_token():
+def get_access_token(config):
     global has_token
     try:
+        logging.info("Attempting to get a previous token...")
         with open("token.json") as file:
             data = json.load(file)
     except FileNotFoundError:
         # get a new token, mildly sketchy logic
+        logging.info("token.json not found, creating...")
         data = {"expires_in": datetime.now() - timedelta(days=3)}
     # if the code will expire in less than an hour, get a new one
     if datetime.strptime(data["expires_in"], "%d/%m/%Y %H:%M:%S") < (datetime.now() + timedelta(hours=1)):
+        logging.info("Token expired, getting a new one:")
         print("Connect to twitch via this link:")
+        scopes = [
+            "moderator:read:chatters",
+            "chat:edit",
+            "chat:read",
+            "moderator:manage:shoutouts"
+        ]
+        logging.info(f"Scopes: {', '.join(scopes)}")
         # print("""https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=28jexim9tw3g8u52evmlqbpob96ymn&redirect_uri=http://localhost:3000&scope=moderator%3Aread%3Achatters%2Bchat%3Aedit%2Bchat%3Aread&state=c3ab8aa609ea11e793ae92361f002671""")
         print(f"""https://id.twitch.tv/oauth2/authorize
         ?response_type=code
         &client_id={config['client_id']}
         &redirect_uri=http://localhost:3000
-        &scope=moderator:read:chatters+chat:edit+chat:read""".replace("\n", "").replace(" ", "").strip())
+        &scope={'+'.join(scopes)}""".replace("\n", "").replace(" ", "").strip())
         print("Waiting...")
+        logging.info("Waiting for user...")
         while not has_token:
             pass
-        response = requests.post("https://id.twitch.tv/oauth2/token", data=f"client_id={config['client_id']}&client_secret={config['client_secret']}&code={token}&grant_type=authorization_code&redirect_uri=http://localhost:3000").json()
-        response["expires_in"] = (datetime.now() + timedelta(seconds=response["expires_in"] - 60)).strftime("%d/%m/%Y %H:%M:%S")
-        with open("token.json", "w") as file:
-            json.dump(response, file)
+        response = requests.post("https://id.twitch.tv/oauth2/token",
+                                 data=f"client_id={config['client_id']}&client_secret={config['client_secret']}&code={token}&grant_type=authorization_code&redirect_uri=http://localhost:3000").json()
+        try:
+            response["expires_in"] = (datetime.now() + timedelta(seconds=response["expires_in"] - 60)).strftime(
+                "%d/%m/%Y %H:%M:%S")
+            with open("token.json", "w") as file:
+                json.dump(response, file)
 
-        return token
+            logging.info(f"New token acquired: {token}")
+            return token
+        except Exception as e:
+            logging.error(f"Error getting token: {str(e)}")
+            logging.error("Most likely invalid client id/secret")
+            raise Exception(f"Error getting token: {str(e)}. Check your client_id and client_secret in config.json!")
     else:
         return data["access_token"]
 
 
-def refresh_token():
-    pass
-
-
 class Bot(commands.Bot):
-    def __init__(self, token, channels=None):
+    def __init__(self, config_file, channels=None):
+        try:
+            with open(config_file) as file:
+                config = json.load(file)
+        except FileNotFoundError:
+            logging.error(f"{config_file} not found!")
+            raise Exception(f"{config_file} not found, please run set_stuff.py with the command 'python set_stuff.py'")
+        tmp = list(config.keys())
+        tmp.sort()
+        if tmp != ['channel', 'client_id', 'client_secret']:
+            with open(config_file, "w") as file:
+                json.dump({"client_id": "", "client_secret": "", "channel": ""}, file, indent=2)
+            raise Exception("Invalid config.json format! Resetting to default...")
+        if config["channel"] == "" or config["client_id"] == "" or config["client_secret"] == "":
+            logging.error("Default config detected, breaking...")
+            raise Exception(
+                "Default config detected, please run set_stuff.py with the command 'python set_stuff.py' to configure the bot")
+
         if channels is None:
             channels = [config['channel']]
 
-        super().__init__(token=token, prefix="!", initial_channels=channels)
+        self._config = config
+        self._token = get_access_token(config)
+        print("Attempting connection...")
+        # sometimes a new token needs a second to actually work :3
+        time.sleep(5)
+        super().__init__(token=self._token, prefix="!", initial_channels=channels)
+        self._user = self._get_user(config["channel"])
+
+    def _get(self, url):
+        logging.debug(f"Getting url: {url}")
+        return requests.get(url, headers={"Authorization": f"Bearer {self._token}",
+                                          "Client-Id": self._config["client_id"]}).json()
+
+    def _get_user(self, username):
+        logging.info(f"Attempting to get user {username}")
+        try:
+            data = self._get(f"https://api.twitch.tv/helix/users?login={username}")["data"][0]
+            logging.info(f"Successfully got user {data['display_name']} with ID {data['id']}")
+            return self.create_user(data["id"], data["display_name"])
+        except IndexError:
+            # user does not exist
+            return None
 
     async def event_ready(self):
+        logging.info(f"Logged in as {self.nick} with ID {self.user_id}")
+        logging.info(f"Connected to: {[channel.name for channel in self.connected_channels]}")
         # We are logged in and ready to chat and use commands...
         print(f'Logged in as @{self.nick}')
         print(f'User id is {self.user_id}')
@@ -136,21 +196,27 @@ class Bot(commands.Bot):
 
     @commands.command()
     async def hello(self, ctx: commands.Context):
+        logging.info(f"!hello called by {ctx.author.name}")
         # cheese
         await ctx.send(f'Hello {ctx.author.name}!')
 
     @commands.command()
     async def ping(self, ctx: commands.Context):
+        logging.info(f"!pong called by {ctx.author.name}")
         # pong
         await ctx.send(f'Pong!')
 
     @commands.command()
     async def addquote(self, ctx: commands.Context):
+        logging.info(f"!addquote called by {ctx.author.name}")
         # if you see this I'll remove it :]
-        if ctx.author.badges.get("vip") or ctx.author.badges.get("mod") or ctx.author.name == "dunno4321" or ctx.author.name == ctx.channel.name:
+        if ctx.author.badges.get("vip") or ctx.author.badges.get(
+                "mod") or ctx.author.name == "dunno4321" or ctx.author.name == ctx.channel.name:
             num_quotes = add_quote(ctx)
+            logging.info(f"Added quote #{num_quotes}")
             await ctx.send(f"Successfully added quote #{num_quotes}")
         else:
+            logging.info(f"{ctx.author.name} does not have permission to use this command!")
             await ctx.send("<3 you dont have permission to use this command <3")
 
     # @commands.command()
@@ -159,6 +225,7 @@ class Bot(commands.Bot):
 
     @commands.command()
     async def quote(self, ctx: commands.Context):
+        logging.info(f"!quote called by {ctx.author.name}")
         msg = ctx.message.content.strip().replace("\U000e0000", "")
         args = msg.split(" ")[1:]
         args = [item.strip() for item in args if len(item.strip()) > 0]
@@ -167,17 +234,20 @@ class Bot(commands.Bot):
             try:
                 number = int(number)
                 if number <= 0:
+                    logging.error(f"{number} must be greater than 0!")
                     await ctx.send("Argument 'number' must be a number greater than 0!")
                 else:
                     with open("quotes.json") as file:
                         quotes = json.load(file)
                     quotes = [quote for quote in quotes if quote["streamer"] == ctx.channel.name]
                     if number > len(quotes):
-                        await ctx.send(f"Argument 'number' out of bounds! Only {len(quotes)} found!" if len(quotes) > 0 else "No quotes found!")
+                        logging.error(f"{number} out of bounds! Only {len(quotes)} found")
+                        await ctx.send(f"Argument 'number' out of bounds! Only {len(quotes)} found!" if len(
+                            quotes) > 0 else "No quotes found!")
                     else:
                         await ctx.send(format_quote(quotes[number - 1]))
             except Exception as e:
-                print(e)
+                logging.error(f"{number} is not a valid number. Error message: {str(e)}")
                 await ctx.send("Argument 'number' is not a valid number!")
         else:
             await ctx.send(random_quote(ctx))
@@ -185,6 +255,7 @@ class Bot(commands.Bot):
     @commands.command()
     async def ads(self, ctx: commands.Context):
         # https://github.com/pixeltris/TwitchAdSolutions
+        logging.info(f"!ads called by {ctx.author.name}")
         if ctx.author.badges.get("vip") or ctx.author.badges.get("mod") or ctx.author.name == ctx.channel.name:
             await ctx.send("https://github.com/pixeltris/TwitchAdSolutions")
         else:
@@ -192,7 +263,8 @@ class Bot(commands.Bot):
 
     @commands.command()
     async def help(self, ctx: commands.Context):
-        msg = """Commands:     
+        logging.info(f"!help called by {ctx.author.name}")
+        msg = """Commands:
 !help: shows this  ||  
 !quote [num]: Shows quote [num] from this streamer. If no number is passed, shows a random quote  ||  
 !hello: says hello back  ||  
@@ -203,17 +275,39 @@ class Bot(commands.Bot):
 !ads (mods/vips only): Sends a link to a Github repository with instructions on how to block Twitch ads"""
         await ctx.send(msg)
 
+    @commands.command(aliases=["so"])
+    async def shoutout(self, ctx: commands.Context, streamer: str):
+        logging.info(f"!shoutout called by {ctx.author.name} to {streamer}")
+        if ctx.author.badges.get("vip") or ctx.author.badges.get("mod") or ctx.author.name == ctx.channel.name:
+            streamer = streamer.strip().replace("@", "")
+            user = self._get_user(streamer)
+            if user is not None:
+                logging.info(f"Streamer ID: {user.id}")
+                await ctx.send(f"Go check out @{user.name} at https://twitch.tv/{user.name} !")
+                # await user.shoutout(self._token, user.id, self._user.id)
+                requests.post(f"https://api.twitch.tv/helix/chat/shoutouts?"
+                              f"from_broadcaster_id={self.user_id}&to_broadcaster_id={user.id}"
+                              f"&moderator_id={self.user_id}".replace(" ", "").replace("\n", ""),
+                              headers={"Authorization": f"Bearer {self._token}",
+                                       "Client-Id": self._config["client_id"]}
+                              )
+                logging.info("Successfully sent shoutout!")
+            else:
+                logging.error(f"User {streamer} not found!")
+                await ctx.send(f"User {streamer} not found!")
+        else:
+            logging.warning(f"User {ctx.author.name} does not have permission to use !shoutout !")
+            await ctx.send("<3 you dont have permission to use this command <3")
+
 
 app_thread = Thread(target=app.run, kwargs={"port": 3000, "host": "0.0.0.0"})
 bot = None
 try:
+    logging.info("Starting Flask thread...")
     app_thread.start()
     # allow time for the Flask info to get put to console before the link
     time.sleep(0.2)
-    token = get_access_token()
-    time.sleep(1)
-    bot = Bot(token, channels=[config["channel"]])
-    print("Attempting connection...")
+    bot = Bot("config.json")
     bot.run()
 except KeyboardInterrupt:
     if bot is not None:
--- set_stuff.py
+++ set_stuff.py
@@ -10,9 +10,9 @@ print("This is where you will input data like Client ID and Client Secret")
 print("This is used to authenticate the bot and enable it to send messages in chat")
 print("All data is stored locally, in the config.json file")
 
-config["client_id"] = input("Client ID: ")
-config["client_secret"] = input("Client secret: ")
-config["channel"] = input("Your channel name: ")
+config["client_id"] = input("Client ID: ").strip()
+config["client_secret"] = input("Client secret: ").strip()
+config["channel"] = input("Your channel name: ").strip()
 
 with open("config.json", "w") as file:
     json.dump(config, file)
